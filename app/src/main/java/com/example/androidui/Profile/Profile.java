011a817bcee7806d6e3ed7214fd929d1e4f605c7
Nam <phnam909@gmail.com>
2021-01-02 16:27:51 +0700

    fix profile active

--- app/src/main/java/com/example/androidui/Profile/Profile.java
+++ app/src/main/java/com/example/androidui/Profile/Profile.java
@@ -1,4 +1,14 @@
 package com.example.androidui.Profile;
 
-public class Profile {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidui.R;
+
+public class Profile extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);}
 }
