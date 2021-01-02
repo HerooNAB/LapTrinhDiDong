1f20a87a6150bb6b47d9462d3d615db8cc526709
HerooNAB <56840074+HerooNAB@users.noreply.github.com>
2021-01-02 11:18:46 +0700

    Binh - Update Service Login + Signup - Call API - SharePrefs

--- app/src/main/java/com/example/androidui/Login1.java
+++ app/src/main/java/com/example/androidui/Login1.java
@@ -47,12 +47,10 @@ public class Login1 extends AppCompatActivity {
         edtPhoneNumber.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
             }
 
             @Override
